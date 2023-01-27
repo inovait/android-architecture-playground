58875eb79f0918a8c7086afce3facbee14668e89
Matej Drobnic <matej.drobnic@inova.si>
2023-01-27 09:13:28 +0100

    ci: add detekt

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,10 @@
+// https://youtrack.jetbrains.com/issue/KTIJ-19369
+// AGP 7.4.0 has a bug where it marks most things as incubating
+@file:Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage")
+
 plugins {
    `kotlin-dsl`
+   alias(libs.plugins.detekt)
 }
 
 repositories {
@@ -8,11 +13,18 @@ repositories {
    gradlePluginPortal()
 }
 
+detekt {
+   config = files("$projectDir/../config/detekt.yml", "$projectDir/../config/detekt-buildSrc.yml")
+}
+
 dependencies {
    implementation(libs.android.agp)
+   implementation(libs.detekt.plugin)
    implementation(libs.kotlin.plugin)
    implementation(libs.versionsCheckerPlugin)
 
    // Workaround to have libs accessible (from https://github.com/gradle/gradle/issues/15383)
    compileOnly(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+
+   detektPlugins(libs.detekt.formatting)
 }
--- buildSrc/src/main/kotlin/checks.gradle.kts
+++ buildSrc/src/main/kotlin/checks.gradle.kts
@@ -2,16 +2,27 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.kotlin.dsl.the
 
+val libs = the<LibrariesForLibs>()
+
 plugins {
    id("com.github.ben-manes.versions")
+   id("io.gitlab.arturbosch.detekt")
 }
 
 tasks.withType<DependencyUpdatesTask> {
    rejectVersionIf {
       candidate.version.contains("alpha", ignoreCase = true) ||
-            candidate.version.contains("beta", ignoreCase = true) ||
-            candidate.version.contains("RC", ignoreCase = true) ||
-            candidate.version.contains("M", ignoreCase = true) ||
-            candidate.version.contains("eap", ignoreCase = true)
+         candidate.version.contains("beta", ignoreCase = true) ||
+         candidate.version.contains("RC", ignoreCase = true) ||
+         candidate.version.contains("M", ignoreCase = true) ||
+         candidate.version.contains("eap", ignoreCase = true)
    }
+}
+
+detekt {
+   config = files("$rootDir/config/detekt.yml")
+}
+
+dependencies {
+   detektPlugins(libs.detekt.formatting)
 }
