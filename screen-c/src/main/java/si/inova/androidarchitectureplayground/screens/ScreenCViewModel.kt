a7c129bdd29a6dd7533d66cb0baca4ac874b2245
Matej Drobnič <matej@matejdro.com>
2023-02-23 12:05:42 +0100

    feat: make ScreenCViewModel into interface

--- screen-c/src/main/java/si/inova/androidarchitectureplayground/screens/ScreenCViewModel.kt
+++ screen-c/src/main/java/si/inova/androidarchitectureplayground/screens/ScreenCViewModel.kt
@@ -1,31 +1,7 @@
 package si.inova.androidarchitectureplayground.screens
 
-import android.util.Log
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.awaitCancellation
-import kotlinx.coroutines.launch
-import si.inova.androidarchitectureplayground.navigation.base.SingleScreenViewModel
-import javax.inject.Inject
-import kotlin.random.Random
+import si.inova.androidarchitectureplayground.navigation.base.ScopedService
 
-class ScreenCViewModel @Inject constructor() : SingleScreenViewModel<ScreenCKey>() {
-   var number by saved(0)
-
-   @OptIn(ExperimentalStdlibApi::class)
-   override fun onServiceRegistered() {
-      Log.d("ViewModel", "got key $key")
-
-      if (number == 0) {
-         number = Random.nextInt()
-      }
-
-      coroutineScope.launch {
-         Log.d("ScreenCViewModel", "Running in ${coroutineContext[CoroutineDispatcher]}")
-         try {
-            awaitCancellation()
-         } finally {
-            Log.d("ScreenCViewModel", "cancelled")
-         }
-      }
-   }
+interface ScreenCViewModel : ScopedService {
+   var number: Int
 }
---
+++ screen-c/src/main/java/si/inova/androidarchitectureplayground/screens/ScreenCViewModelImpl.kt
@@ -0,0 +1,34 @@
+package si.inova.androidarchitectureplayground.screens
+
+import android.util.Log
+import com.squareup.anvil.annotations.ContributesBinding
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.launch
+import si.inova.androidarchitectureplayground.di.SimpleStackActivityScope
+import si.inova.androidarchitectureplayground.navigation.base.SingleScreenViewModel
+import javax.inject.Inject
+import kotlin.random.Random
+
+@ContributesBinding(SimpleStackActivityScope::class, boundType = ScreenCViewModel::class)
+class ScreenCViewModelImpl @Inject constructor() : SingleScreenViewModel<ScreenCKey>(), ScreenCViewModel {
+   override var number by saved(0)
+
+   @OptIn(ExperimentalStdlibApi::class)
+   override fun onServiceRegistered() {
+      Log.d("ViewModel", "got key $key")
+
+      if (number == 0) {
+         number = Random.nextInt()
+      }
+
+      coroutineScope.launch {
+         Log.d("ScreenCViewModel", "Running in ${coroutineContext[CoroutineDispatcher]}")
+         try {
+            awaitCancellation()
+         } finally {
+            Log.d("ScreenCViewModel", "cancelled")
+         }
+      }
+   }
+}
