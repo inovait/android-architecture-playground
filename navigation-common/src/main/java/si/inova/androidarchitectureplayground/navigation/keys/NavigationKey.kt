fcffe9491a9055079cd86561ed13f53fbfd76f50
Matej Drobnic <matej.drobnic@inova.si>
2023-02-24 07:45:48 +0100

    refactor: make NavigationKey parcelable

--- navigation-common/src/main/java/si/inova/androidarchitectureplayground/navigation/keys/NavigationKey.kt
+++ navigation-common/src/main/java/si/inova/androidarchitectureplayground/navigation/keys/NavigationKey.kt
@@ -1,7 +1,8 @@
 package si.inova.androidarchitectureplayground.navigation.keys
 
+import android.os.Parcelable
 import com.zhuinden.simplestack.Backstack
 
-abstract class NavigationKey {
+abstract class NavigationKey : Parcelable {
    abstract fun performNavigation(backstack: Backstack)
 }
--- navigation-common/src/main/java/si/inova/androidarchitectureplayground/navigation/keys/ReplaceHistoryKey.kt
+++ navigation-common/src/main/java/si/inova/androidarchitectureplayground/navigation/keys/ReplaceHistoryKey.kt
@@ -2,7 +2,9 @@ package si.inova.androidarchitectureplayground.navigation.keys
 
 import com.zhuinden.simplestack.Backstack
 import com.zhuinden.simplestack.StateChange
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 class ReplaceHistoryKey(vararg val history: ScreenKey) : NavigationKey(), InitialNavigationKey {
    override fun performNavigation(backstack: Backstack) {
       backstack.setHistory(history.toList(), StateChange.REPLACE)
