ee1045208e7191ff86ef3eef740aa09053d084cd
Matej Drobnič <matej@matejdro.com>
2023-03-02 07:21:26 +0100

    refactor: use rememberBackstack on MainActivity

--- app/src/main/java/si/inova/androidarchitectureplayground/MainActivity.kt
+++ app/src/main/java/si/inova/androidarchitectureplayground/MainActivity.kt
@@ -1,22 +1,19 @@
 package si.inova.androidarchitectureplayground
 
 import android.content.Intent
-import android.content.res.Resources
 import android.net.Uri
 import android.os.Bundle
-import android.view.Window
-import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.fragment.app.FragmentActivity
 import com.deliveryhero.whetstone.Whetstone
 import com.deliveryhero.whetstone.activity.ContributesActivityInjector
 import com.zhuinden.simplestack.AsyncStateChanger
 import com.zhuinden.simplestack.History
-import com.zhuinden.simplestack.navigator.Navigator
 import si.inova.androidarchitectureplayground.di.NavigationStackComponent
 import si.inova.androidarchitectureplayground.navigation.base.DeepLinkHandler
 import si.inova.androidarchitectureplayground.navigation.base.Screen
@@ -27,6 +24,7 @@ import si.inova.androidarchitectureplayground.simplestack.BackstackProvider
 import si.inova.androidarchitectureplayground.simplestack.ComposeStateChanger
 import si.inova.androidarchitectureplayground.simplestack.MyScopedServices
 import si.inova.androidarchitectureplayground.simplestack.NavigationContextImpl
+import si.inova.androidarchitectureplayground.simplestack.rememberBackstack
 import si.inova.androidarchitectureplayground.ui.theme.AndroidArchitecturePlaygroundTheme
 import javax.inject.Inject
 import javax.inject.Provider
@@ -34,15 +32,10 @@ import javax.inject.Provider
 @ContributesActivityInjector
 class MainActivity : FragmentActivity() {
    @Inject
-   lateinit var injectedResources: Resources
-
-   @Inject
    lateinit var navigationStackComponentFactory: NavigationStackComponent.Factory
 
    @Inject
    lateinit var deepLinkHandlers: Set<@JvmSuppressWildcards DeepLinkHandler>
-
-   lateinit var composeStateChanger: ComposeStateChanger
 
    lateinit var navigator: si.inova.androidarchitectureplayground.navigation.Navigator
 
@@ -53,35 +46,42 @@ class MainActivity : FragmentActivity() {
       super.onCreate(savedInstanceState)
       Whetstone.inject(this)
 
-      lateinit var screenFactories: Map<@JvmSuppressWildcards Class<*>, @JvmSuppressWildcards Provider<Screen<*>>>
-
-      composeStateChanger = ComposeStateChanger(screenFactories = lazy { screenFactories })
-
-      val scopedServices = MyScopedServices()
-
       val deepLinkTarget = intent?.data?.let { getDeepLinkTarget(it, startup = true) }
       var initialHistory: List<ScreenKey> = History.of(ScreenAKey)
       if (deepLinkTarget != null) {
          initialHistory = deepLinkTarget.performNavigation(initialHistory, navigationContext).newBackstack
       }
 
-      val backstack = Navigator.configure()
-         .setStateChanger(AsyncStateChanger(composeStateChanger))
-         .setScopedServices(scopedServices)
-         .setDeferredInitialization(true)
-         .install(this, findViewById(Window.ID_ANDROID_CONTENT), initialHistory)
-
-      val activityComponent = navigationStackComponentFactory.create(backstack, backstack)
-      screenFactories = activityComponent.screenFactories()
-      scopedServices.scopedServicesFactories = activityComponent.scopedServicesFactories()
-      scopedServices.scopedServicesKeys = activityComponent.scopedServicesKeys()
-      navigator = activityComponent.navigator()
-
-      Navigator.executeDeferredInitialization(this)
-
-      onBackPressedDispatcher.addCallback(simpleStackBackPressedCallback)
-
       setContent {
+         var screenFactories: Map<@JvmSuppressWildcards Class<*>, @JvmSuppressWildcards Provider<Screen<*>>>? = null
+
+         val composeStateChanger = remember {
+            ComposeStateChanger(screenFactories = lazy { requireNotNull(screenFactories) })
+         }
+
+         val asyncStateChanger = remember() { AsyncStateChanger(composeStateChanger) }
+         val backstack = rememberBackstack(asyncStateChanger) {
+            val scopedServices = MyScopedServices()
+
+            val backstack = createBackstack(
+               initialHistory,
+               scopedServices = scopedServices
+            )
+
+            val activityComponent = navigationStackComponentFactory.create(backstack, backstack)
+            screenFactories = activityComponent.screenFactories()
+            scopedServices.scopedServicesFactories = activityComponent.scopedServicesFactories()
+            scopedServices.scopedServicesKeys = activityComponent.scopedServicesKeys()
+            navigator = activityComponent.navigator()
+
+            backstack
+         }
+
+         if (screenFactories == null) {
+            val component = navigationStackComponentFactory.create(backstack, backstack)
+            screenFactories = component.screenFactories()
+         }
+
          AndroidArchitecturePlaygroundTheme {
             // A surface container using the 'background' color from the theme
             Surface(
@@ -112,17 +112,4 @@ class MainActivity : FragmentActivity() {
          .mapNotNull<@JvmSuppressWildcards DeepLinkHandler, NavigationInstruction> { it.handleDeepLink(uri, startup) }
          .firstOrNull()
    }
-
-   private val simpleStackBackPressedCallback =
-      object : OnBackPressedCallback(true) {
-         override fun handleOnBackPressed() {
-            if (!Navigator.onBackPressed(this@MainActivity)) {
-               // Hack until https://github.com/Zhuinden/simple-stack/issues/259 is resolved
-               this.remove()
-               @Suppress("DEPRECATION")
-               onBackPressed()
-               this@MainActivity.onBackPressedDispatcher.addCallback(this)
-            }
-         }
-      }
 }
