6aee6dcc66fc5f00203134589324f5c69c5ac850
Matej Drobnič <matej@matejdro.com>
2023-03-02 07:14:47 +0100

    fix: use injected main backstack
    this will allow nested-nested screens to still navigate with main backstack

--- navigation-common/src/main/java/si/inova/androidarchitectureplayground/navigation/nested/NestedNavigator.kt
+++ navigation-common/src/main/java/si/inova/androidarchitectureplayground/navigation/nested/NestedNavigator.kt
@@ -3,25 +3,26 @@ package si.inova.androidarchitectureplayground.navigation.nested
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import com.zhuinden.simplestack.AsyncStateChanger
+import com.zhuinden.simplestack.Backstack
 import com.zhuinden.simplestack.History
+import si.inova.androidarchitectureplayground.di.MainNavigation
 import si.inova.androidarchitectureplayground.di.NavigationInjection
 import si.inova.androidarchitectureplayground.navigation.base.Screen
 import si.inova.androidarchitectureplayground.navigation.keys.ScreenKey
 import si.inova.androidarchitectureplayground.simplestack.BackstackProvider
 import si.inova.androidarchitectureplayground.simplestack.ComposeStateChanger
-import si.inova.androidarchitectureplayground.simplestack.LocalBackstack
 import si.inova.androidarchitectureplayground.simplestack.MyScopedServices
 import si.inova.androidarchitectureplayground.simplestack.rememberBackstack
 import javax.inject.Inject
 import javax.inject.Provider
 
 class NestedNavigator @Inject constructor(
-   private val navigationStackComponentFactory: NavigationInjection.Factory
+   private val navigationStackComponentFactory: NavigationInjection.Factory,
+   @MainNavigation
+   private val mainBackstack: Backstack
 ) {
    @Composable
    fun NestedNavigation(id: String = "SINGLE", initialKeys: () -> History<ScreenKey>) {
-      val parentBackstack = LocalBackstack.current
-
       var screenFactories: Map<@JvmSuppressWildcards Class<*>, @JvmSuppressWildcards Provider<Screen<*>>>? = null
 
       val stateChanger = remember {
@@ -35,7 +36,7 @@ class NestedNavigator @Inject constructor(
             scopedServices = scopedServices
          )
 
-         val component = navigationStackComponentFactory.create(backstack, parentBackstack)
+         val component = navigationStackComponentFactory.create(backstack, mainBackstack)
 
          screenFactories = component.screenFactories()
          scopedServices.scopedServicesFactories = component.scopedServicesFactories()
@@ -45,7 +46,7 @@ class NestedNavigator @Inject constructor(
       }
 
       if (screenFactories == null) {
-         val component = navigationStackComponentFactory.create(backstack, parentBackstack)
+         val component = navigationStackComponentFactory.create(backstack, mainBackstack)
          screenFactories = component.screenFactories()
       }
 
