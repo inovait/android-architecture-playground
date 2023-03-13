6e82f5f218d138d0797f5f91d3f788c39c902ddb
Matej Drobnic <matej.drobnic@inova.si>
2023-03-13 15:13:36 +0100

    refactor: change BlinkingPrevention into class
    This allows us to refactor out and avoid LongMethod and ComplexMethod warnings

--- common/pure/src/main/java/si/inova/androidarchitectureplayground/common/flow/BlinkingPrevention.kt
+++ common/pure/src/main/java/si/inova/androidarchitectureplayground/common/flow/BlinkingPrevention.kt
@@ -2,9 +2,11 @@ package si.inova.androidarchitectureplayground.common.flow
 
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.produceIn
 import kotlinx.coroutines.isActive
+import kotlinx.coroutines.selects.SelectBuilder
 import kotlinx.coroutines.selects.select
 import si.inova.androidarchitectureplayground.common.outcome.CauseException
 import si.inova.androidarchitectureplayground.common.outcome.Outcome
@@ -19,14 +21,25 @@ import si.inova.androidarchitectureplayground.common.outcome.Outcome
  * * Once [Outcome.Progress] will be emitted, it will not emit any non-progress
  *   state for at least [keepLoadingActiveForAtLeastMs] milliseconds
  */
-@Suppress("LongMethod", "CyclomaticComplexMethod") // Splitting this would cause even more complexity.
 fun <T> Flow<Outcome<T>>.withBlinkingPrevention(
    waitThisLongToShowLoadingMs: Long = 100,
    keepLoadingActiveForAtLeastMs: Long = 500,
    doNotWaitForInterimLoadings: Boolean = false
 ): Flow<Outcome<T>> {
-   val upstream = this
+   return BlinkingPrevention(
+      this,
+      waitThisLongToShowLoadingMs,
+      keepLoadingActiveForAtLeastMs,
+      doNotWaitForInterimLoadings
+   )
+}
 
+private class BlinkingPrevention<T>(
+   private val upstream: Flow<Outcome<T>>,
+   private val waitThisLongToShowLoadingMs: Long = 100,
+   private val keepLoadingActiveForAtLeastMs: Long = 500,
+   private val doNotWaitForInterimLoadings: Boolean = false
+) : Flow<Outcome<T>> {
    var waitingToSeeIfLoadingJustFlashes = false
    var waitingForProlongedLoadingToFinish = false
    var lastData: T? = null
@@ -35,77 +48,93 @@ fun <T> Flow<Outcome<T>>.withBlinkingPrevention(
    var gotSuccessDuringLoading = false
    var gotAtLeastOneSuccess = false
 
-   return flow {
-      coroutineScope {
-         val upstreamChannel = upstream.produceIn(this)
+   override suspend fun collect(collector: FlowCollector<Outcome<T>>) {
+      flow {
+         coroutineScope {
+            val upstreamChannel = upstream.produceIn(this)
 
-         while (isActive) {
-            select<Unit> {
-               upstreamChannel.onReceive {
-                  when (it) {
-                     is Outcome.Progress -> {
-                        lastData = it.data
-                        lastProgress = it.progress
-
-                        if (waitingForProlongedLoadingToFinish || (gotAtLeastOneSuccess && doNotWaitForInterimLoadings)) {
-                           emit(Outcome.Progress(lastData, lastProgress))
-                        } else if (waitingToSeeIfLoadingJustFlashes) {
-                           // Do nothing, only update current data
-                        } else {
-                           waitingToSeeIfLoadingJustFlashes = true
-                        }
-                     }
-
-                     is Outcome.Success -> {
-                        gotAtLeastOneSuccess = true
-
-                        if (waitingForProlongedLoadingToFinish) {
+            while (isActive) {
+               select<Unit> {
+                  upstreamChannel.onReceive {
+                     when (it) {
+                        is Outcome.Progress -> {
                            lastData = it.data
-                           emit(Outcome.Progress(lastData, lastProgress))
-                           gotSuccessDuringLoading = true
-                        } else {
-                           waitingToSeeIfLoadingJustFlashes = false
-                           emit(it)
-                        }
-                     }
+                           lastProgress = it.progress
 
-                     is Outcome.Error -> {
-                        if (waitingForProlongedLoadingToFinish) {
-                           lastData = it.data
-                           lastError = it.exception
-                           emit(Outcome.Progress(lastData, lastProgress))
-                        } else {
-                           waitingToSeeIfLoadingJustFlashes = false
-                           lastError = null
-                           emit(it)
+                           if (waitingForProlongedLoadingToFinish || (gotAtLeastOneSuccess && doNotWaitForInterimLoadings)) {
+                              emit(Outcome.Progress(lastData, lastProgress))
+                           } else if (waitingToSeeIfLoadingJustFlashes) {
+                              // Do nothing, only update current data
+                           } else {
+                              waitingToSeeIfLoadingJustFlashes = true
+                           }
+                        }
+
+                        is Outcome.Success -> {
+                           gotAtLeastOneSuccess = true
+
+                           if (waitingForProlongedLoadingToFinish) {
+                              lastData = it.data
+                              emit(Outcome.Progress(lastData, lastProgress))
+                              gotSuccessDuringLoading = true
+                           } else {
+                              waitingToSeeIfLoadingJustFlashes = false
+                              emit(it)
+                           }
+                        }
+
+                        is Outcome.Error -> {
+                           if (waitingForProlongedLoadingToFinish) {
+                              lastData = it.data
+                              lastError = it.exception
+                              emit(Outcome.Progress(lastData, lastProgress))
+                           } else {
+                              waitingToSeeIfLoadingJustFlashes = false
+                              lastError = null
+                              emit(it)
+                           }
                         }
                      }
                   }
-               }
 
-               if (waitingToSeeIfLoadingJustFlashes) {
-                  onTimeout(waitThisLongToShowLoadingMs) {
-                     emit(Outcome.Progress(lastData, lastProgress))
-                     waitingToSeeIfLoadingJustFlashes = false
-                     waitingForProlongedLoadingToFinish = true
+                  if (waitingToSeeIfLoadingJustFlashes) {
+                     showLoadingAfterTimeout(this, this@flow)
                   }
-               }
 
-               if (waitingForProlongedLoadingToFinish) {
-                  onTimeout(keepLoadingActiveForAtLeastMs) {
-                     val currentLastError = lastError
-                     if (currentLastError != null) {
-                        emit(Outcome.Error(currentLastError, lastData))
-                        lastError = null
-                     } else if (gotSuccessDuringLoading) {
-                        emit(Outcome.Success(requireNotNull(lastData)))
-                        gotSuccessDuringLoading = false
-                     }
-                     waitingForProlongedLoadingToFinish = false
+                  if (waitingForProlongedLoadingToFinish) {
+                     continueAfterLoadingFinishes(this, this@flow)
                   }
                }
             }
          }
+      }.collect(collector)
+   }
+
+   private fun continueAfterLoadingFinishes(
+      selectBuilder: SelectBuilder<Unit>,
+      flowCollector: FlowCollector<Outcome<T>>
+   ) {
+      selectBuilder.onTimeout(keepLoadingActiveForAtLeastMs) {
+         val currentLastError = lastError
+         if (currentLastError != null) {
+            flowCollector.emit(Outcome.Error(currentLastError, lastData))
+            lastError = null
+         } else if (gotSuccessDuringLoading) {
+            flowCollector.emit(Outcome.Success(requireNotNull(lastData)))
+            gotSuccessDuringLoading = false
+         }
+         waitingForProlongedLoadingToFinish = false
+      }
+   }
+
+   private fun showLoadingAfterTimeout(
+      selectBuilder: SelectBuilder<Unit>,
+      flowCollector: FlowCollector<Outcome<T>>
+   ) {
+      selectBuilder.onTimeout(waitThisLongToShowLoadingMs) {
+         flowCollector.emit(Outcome.Progress(lastData, lastProgress))
+         waitingToSeeIfLoadingJustFlashes = false
+         waitingForProlongedLoadingToFinish = true
       }
    }
 }
