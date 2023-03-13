188c2ce29d0a666ceca260e5c92f2600d4edc39a
Matej Drobnic <matej.drobnic@inova.si>
2023-03-13 09:27:28 +0100

    refactor: turn ProductListViewModel into interface

--- products/ui/src/main/java/si/inova/androidarchitectureplayground/products/ui/ProductListViewModel.kt
+++ products/ui/src/main/java/si/inova/androidarchitectureplayground/products/ui/ProductListViewModel.kt
@@ -1,31 +1,13 @@
 package si.inova.androidarchitectureplayground.products.ui
 
 import androidx.compose.runtime.Stable
-import com.androidarchitectureplayground.products.data.ProductsRepository
 import com.androidarchitectureplayground.products.data.model.ProductDto
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import si.inova.androidarchitectureplayground.common.outcome.CoroutineResourceManager
 import si.inova.androidarchitectureplayground.common.outcome.Outcome
-import si.inova.androidarchitectureplayground.navigation.base.SingleScreenViewModel
-import si.inova.androidarchitectureplayground.screens.ProductListScreenKey
-import javax.inject.Inject
+import si.inova.androidarchitectureplayground.navigation.base.ScopedService
 
 @Stable
-class ProductListViewModel @Inject constructor(
-   private val resources: CoroutineResourceManager,
-   private val productsRepository: ProductsRepository
-) : SingleScreenViewModel<ProductListScreenKey>(resources.scope) {
-   private val _products = MutableStateFlow<Outcome<List<ProductDto>>>(Outcome.Progress())
-   val products: StateFlow<Outcome<List<ProductDto>>> = _products
-
-   override fun onServiceRegistered() {
-      loadProducts()
-   }
-
-   private fun loadProducts() {
-      resources.launchResourceControlTask(_products) {
-         emitAll(productsRepository.getProducts())
-      }
-   }
+interface ProductListViewModel : ScopedService {
+   val products: StateFlow<Outcome<List<ProductDto>>>
+   fun loadProducts()
 }
---
+++ products/ui/src/main/java/si/inova/androidarchitectureplayground/products/ui/ProductListViewModelImpl.kt
@@ -0,0 +1,32 @@
+package si.inova.androidarchitectureplayground.products.ui
+
+import androidx.compose.runtime.Stable
+import com.androidarchitectureplayground.products.data.ProductsRepository
+import com.androidarchitectureplayground.products.data.model.ProductDto
+import com.deliveryhero.whetstone.app.ApplicationScope
+import com.squareup.anvil.annotations.ContributesBinding
+import kotlinx.coroutines.flow.MutableStateFlow
+import si.inova.androidarchitectureplayground.common.outcome.CoroutineResourceManager
+import si.inova.androidarchitectureplayground.common.outcome.Outcome
+import si.inova.androidarchitectureplayground.navigation.base.SingleScreenViewModel
+import si.inova.androidarchitectureplayground.screens.ProductListScreenKey
+import javax.inject.Inject
+
+@Stable
+@ContributesBinding(ApplicationScope::class, ProductListViewModel::class)
+class ProductListViewModelImpl @Inject constructor(
+   private val resources: CoroutineResourceManager,
+   private val productsRepository: ProductsRepository
+) : SingleScreenViewModel<ProductListScreenKey>(resources.scope), ProductListViewModel {
+   override val products = MutableStateFlow<Outcome<List<ProductDto>>>(Outcome.Progress())
+
+   override fun onServiceRegistered() {
+      loadProducts()
+   }
+
+   override fun loadProducts() {
+      resources.launchResourceControlTask(products) {
+         emitAll(productsRepository.getProducts())
+      }
+   }
+}
