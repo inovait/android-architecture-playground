7f281bfb3be5867549328df8aee3b7b8fa1ebd6c
Matej Drobnic <matej.drobnic@inova.si>
2023-03-27 10:11:11 +0200

    chore: update to Gradle 8

--- buildSrc/src/main/kotlin/android-module-commons.gradle.kts
+++ buildSrc/src/main/kotlin/android-module-commons.gradle.kts
@@ -12,12 +12,12 @@ commonAndroid {
    compileSdk = 33
 
    compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_1_8
-      targetCompatibility = JavaVersion.VERSION_1_8
+      sourceCompatibility = JavaVersion.VERSION_11
+      targetCompatibility = JavaVersion.VERSION_11
    }
 
    commonKotlinOptions {
-      jvmTarget = "1.8"
+      jvmTarget = "11"
    }
 
    defaultConfig {
