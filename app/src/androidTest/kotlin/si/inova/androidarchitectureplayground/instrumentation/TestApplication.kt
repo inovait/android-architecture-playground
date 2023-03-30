32a785eede2d4612e72b80dc086820e003a8cd71
Matej Drobnič <matej@matejdro.com>
2023-03-30 11:16:43 +0200

    test: fix TestApplication crash

--- app/src/androidTest/kotlin/si/inova/androidarchitectureplayground/instrumentation/TestApplication.kt
+++ app/src/androidTest/kotlin/si/inova/androidarchitectureplayground/instrumentation/TestApplication.kt
@@ -1,7 +1,9 @@
 package si.inova.androidarchitectureplayground.instrumentation
 
+import com.deliveryhero.whetstone.app.ContributesAppInjector
 import si.inova.androidarchitectureplayground.MyApplication
 
+@ContributesAppInjector(generateAppComponent = false)
 class TestApplication : MyApplication() {
    override val applicationComponent: TestAppComponent by lazy {
       DaggerTestAppComponent.factory().create(this)
