7f02a3f79f53883f84e85d5146f40c293f669e11
Matej Drobnič <matej@matejdro.com>
2023-03-30 09:39:45 +0200

    chore: fix pre-commit hook on Gradle 8

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -36,5 +36,5 @@ tasks.register("pre-commit-hook", Copy::class) {
 }
 
 afterEvaluate {
-   tasks.getByName("check").dependsOn("pre-commit-hook")
+   tasks.getByName("jar").dependsOn("pre-commit-hook")
 }
