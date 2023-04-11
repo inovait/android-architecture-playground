7afac9ed39a815aa94d945d3db657e8656577836
Matej Drobnic <matej.drobnic@inova.si>
2023-04-11 12:16:27 +0200

    refactor: move automatic toml update to kotlinova

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
    implementation(libs.anvil)
    implementation(libs.detekt.plugin)
    implementation(libs.kotlin.plugin)
+   implementation(libs.kotlinova.gradle)
    implementation(libs.moduleGraphAssert)
    implementation(libs.moshi.ir)
    implementation(libs.orgJson)
--- buildSrc/src/main/kotlin/app-module.gradle.kts
+++ buildSrc/src/main/kotlin/app-module.gradle.kts
@@ -6,12 +6,22 @@ plugins {
    id("com.android.application")
    id("android-module-commons")
    id("kotlin-kapt")
-   id("toml-update")
+   id("toml-version-bump")
    id("com.jraska.module.graph.assertion")
 }
 
 anvil {
    syncGeneratedSources.set(true)
+}
+
+tomlVersionBump {
+   versionReportFiles.set(
+      fileTree(rootDir).apply {
+         include("**/build/dependencyUpdates/versions.json")
+      }
+   )
+
+   tomlFile.set(File(rootDir, "config/libs.toml"))
 }
 
 moduleGraphAssert {
