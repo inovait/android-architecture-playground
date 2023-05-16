349d1303930ca2abf71ea1f7564f92112217a2c4
Matej Drobnic <matej.drobnic@inova.si>
2023-05-16 08:33:19 +0200

    fix(user-data): init moshi on the background thread

--- common-retrofit/src/main/kotlin/si/inova/androidarchitectureplayground/network/exceptions/DefaultErrorHandler.kt
+++ common-retrofit/src/main/kotlin/si/inova/androidarchitectureplayground/network/exceptions/DefaultErrorHandler.kt
@@ -6,12 +6,13 @@ import si.inova.kotlinova.core.outcome.CauseException
 import si.inova.kotlinova.retrofit.callfactory.ErrorHandler
 import si.inova.kotlinova.retrofit.moshi.fromJson
 import javax.inject.Inject
+import javax.inject.Provider
 
 class DefaultErrorHandler @Inject constructor(
-   private val moshi: Moshi
+   private val moshi: Provider<Moshi>
 ) : ErrorHandler {
    override fun generateExceptionFromErrorBody(response: Response<*>, parentException: Exception): CauseException? {
-      val errorMessage = response.errorBody()?.source()?.let { moshi.fromJson<ErrorResponse>(it).message } ?: "Unknown"
+      val errorMessage = response.errorBody()?.source()?.let { moshi.get().fromJson<ErrorResponse>(it).message } ?: "Unknown"
       return BackendException(errorMessage, parentException)
    }
 }
