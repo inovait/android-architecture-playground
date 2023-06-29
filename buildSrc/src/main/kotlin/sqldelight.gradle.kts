9bec886b589e34f38270bfd3dbb8c32c41202dd7
Matej Drobnič <matej@matejdro.com>
2023-06-29 09:57:57 +0200

    chore: improve sqldelight detekt exclusion

--- buildSrc/src/main/kotlin/sqldelight.gradle.kts
+++ buildSrc/src/main/kotlin/sqldelight.gradle.kts
@@ -9,9 +9,10 @@ plugins {
 afterEvaluate {
    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
       // SQLDelight marks generated files incorrectly as regular source files. We must exclude it manually.
-      // Due to this, we force all sqdelight generated files to be in a `*.sqlidelight.generated` package.
       // This is a workaround for the https://github.com/cashapp/sqldelight/issues/4157.
-      exclude("**/sqldelight/generated/**")
+      exclude {
+         it.file.absolutePath.contains("build/")
+      }
    }
 }
 
