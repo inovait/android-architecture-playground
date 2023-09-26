eeb0bda8f5715103db0599248ea6be61597138d1
Matej Drobnič <matej@matejdro.com>
2023-09-26 07:10:31 +0200

    feat: add logger sample

--- login/ui/src/main/kotlin/si/inova/androidarchitectureplayground/login/ui/FlowDemo.kt
+++ login/ui/src/main/kotlin/si/inova/androidarchitectureplayground/login/ui/FlowDemo.kt
@@ -1,11 +1,14 @@
 package si.inova.androidarchitectureplayground.login.ui
 
+import android.content.Context
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import si.inova.kotlinova.core.logging.logcat
+import java.io.File
 
 object FlowDemo {
    suspend fun CoroutineScope.start() {
@@ -25,6 +28,24 @@ object FlowDemo {
          logcat { "Emit $number" }
          channel.send(number)
          logcat { "Done emitting $number" }
+      }
+   }
+}
+
+class Logger(private val context: Context, private val coroutineScope: CoroutineScope) {
+   private val channel = Channel<String>(Channel.BUFFERED)
+
+   fun log(text: String) {
+      channel.trySend(text)
+   }
+
+   init {
+      coroutineScope.launch(Dispatchers.Default) {
+         val file = File(context.filesDir, "log.txt")
+
+         channel.consumeEach {
+            file.appendText(it)
+         }
       }
    }
 }
