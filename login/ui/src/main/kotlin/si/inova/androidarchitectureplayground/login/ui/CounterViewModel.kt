e28f3d9a8cbf27fcadd712145ca21fcd86b587ca
Matej Drobnič <matej@matejdro.com>
2023-09-26 07:30:16 +0200

    feat: add paused select handling

--- login/ui/src/main/kotlin/si/inova/androidarchitectureplayground/login/ui/CounterViewModel.kt
+++ login/ui/src/main/kotlin/si/inova/androidarchitectureplayground/login/ui/CounterViewModel.kt
@@ -1,8 +1,10 @@
 package si.inova.androidarchitectureplayground.login.ui
 
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.produceIn
+import kotlinx.coroutines.selects.onTimeout
+import kotlinx.coroutines.selects.select
 import si.inova.kotlinova.core.outcome.CoroutineResourceManager
 import si.inova.kotlinova.core.outcome.Outcome
 import si.inova.kotlinova.navigation.services.CoroutineScopedService
@@ -17,9 +19,29 @@ class CounterViewModel @Inject constructor(
    val paused = MutableStateFlow<Boolean>(false)
 
    fun startCounting() = resources.launchResourceControlTask(_counter) {
+      var enabled = true
+      val pausedChannel = paused.produceIn(this)
+
       for (i in 10 downTo 0) {
-         emit(Outcome.Success(i))
-         delay(500)
+         while (true) {
+            val emitted = select<Boolean> {
+               pausedChannel.onReceive { paused ->
+                  enabled = !paused
+                  false
+               }
+
+               if (enabled) {
+                  onTimeout(500) {
+                     emit(Outcome.Success(i))
+                     true
+                  }
+               }
+            }
+
+            if (emitted) {
+               break
+            }
+         }
       }
    }
 
