c287c30824c6f3a36341b940b6da65f3eec0aa2e
Matej Drobnic <matej.drobnic@inova.si>
2023-10-10 09:46:57 +0200

    chore: bump dependencies

--- app/src/main/kotlin/si/inova/androidarchitectureplayground/di/AppModule.kt
+++ app/src/main/kotlin/si/inova/androidarchitectureplayground/di/AppModule.kt
@@ -18,13 +18,15 @@ import si.inova.kotlinova.core.time.TimeProvider
 @Module
 abstract class AppModule {
    @Binds
-   abstract fun Application.bindToContext(): Context
+   abstract fun bindToContext(application: Application): Context
 
    @Binds
-   abstract fun AndroidTimeProvider.bindToTimeProvider(): TimeProvider
+   abstract fun bindToTimeProvider(androidTimeProvider: AndroidTimeProvider): TimeProvider
 
    @Binds
-   abstract fun AndroidDateTimeFormatterImpl.bindToAndroidDateTimeFormatter(): AndroidDateTimeFormatter
+   abstract fun bindToAndroidDateTimeFormatter(
+      androidDateTimeFormatterImpl: AndroidDateTimeFormatterImpl
+   ): AndroidDateTimeFormatter
 
    @Module
    companion object {
