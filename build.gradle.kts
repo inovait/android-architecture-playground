ac160c6cc0791866ec51b4454469fd3dca4a94df
Matej Drobnic <matej.drobnic@inova.si>
2023-12-08 07:31:01 +0100

    build: merge static analysis issues into sarif reports
    (cherry picked from commit 2c29e44d91569fc41416c1d125754559df18e95a)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,53 @@
-// https://youtrack.jetbrains.com/issue/KTIJ-19369
-@file:Suppress("DSL_SCOPE_VIOLATION")
+import com.android.build.gradle.internal.lint.AndroidLintTask
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 
-// This file is intentionally empty. We should not use any allprojects {} calls, to conform with future project isolation
-// See https://gradle.github.io/configuration-cache/#project_isolation
+val reportMerge by tasks.registering(ReportMergeTask::class) {
+   output.set(File(rootProject.rootDir, "merge.sarif"))
+
+   doFirst {
+      output.get().asFile.delete()
+   }
+}
+
+// This violates build isolation but we are forced to use it for performance reasons
+// Waiting for https://github.com/gradle/gradle/issues/25179 for a possible workaround
+// Please do not add anything else to the subprojects {}
+val inputs = ArrayList<Provider<RegularFile>>()
+
+subprojects {
+   val configuredDetektTasks = ArrayList<Detekt>()
+   val configuredLintTasks = ArrayList<AndroidLintTask>()
+
+   // Hack: Use configureEach to get list of currently executing tasks, to ensure we do not
+
+   tasks.withType<Detekt>().configureEach { configuredDetektTasks.add(this) }
+   tasks.withType<AndroidLintTask>().configureEach { configuredLintTasks.add(this) }
+
+   reportMerge {
+      input.from("buildSrc/build/reports/detekt/detekt.sarif")
+
+      input.from(
+         provider { configuredDetektTasks }.flatMap { list ->
+            provider {
+               list.map {
+                  it.sarifReportFile.get()
+               }
+            }
+         }
+      )
+
+      input.from(
+         provider { configuredLintTasks }.flatMap { list ->
+            provider {
+               list.map { lintTask ->
+                  val variant = lintTask.variantInputs.name.get()
+                  lintTask.project.layout.buildDirectory.file(
+                     "reports/lint-results-$variant.sarif"
+                  ).get()
+               }
+            }
+         }
+      )
+   }
+}
