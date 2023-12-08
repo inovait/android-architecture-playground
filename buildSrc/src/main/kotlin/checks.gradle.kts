bbf89b5f8d7c5554b6d7d986843a0b36a0b49aa4
Matej Drobnic <matej.drobnic@inova.si>
2023-12-08 08:35:21 +0100

    build: fix sarif reports for android lint
    (cherry picked from commit 98a322e7d923fdc74ef9b656452cfb221ea57d3a)

--- buildSrc/src/main/kotlin/checks.gradle.kts
+++ buildSrc/src/main/kotlin/checks.gradle.kts
@@ -17,6 +17,7 @@ if (isAndroidProject()) {
          abortOnError = true
 
          warningsAsErrors = true
+         sarifReport = true
       }
    }
 }
@@ -47,6 +48,10 @@ tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
       sarif.required.set(true)
    }
 
+   finalizedBy(":reportMerge")
+}
+
+tasks.withType<com.android.build.gradle.internal.lint.AndroidLintTask>().configureEach {
    finalizedBy(":reportMerge")
 }
 
