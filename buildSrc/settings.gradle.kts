94713511ca4f06d6d7221b81e2eebc66b18729bb
Matej Drobnic <matej.drobnic@inova.si>
2023-12-11 09:39:53 +0100

    chore: remove KTIJ-19369 notice
    issue has been fixed since

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,7 +1,3 @@
-// https://youtrack.jetbrains.com/issue/KTIJ-19369
-// AGP 7.4.0 has a bug where it marks most things as incubating
-@file:Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage")
-
 dependencyResolutionManagement {
    versionCatalogs {
       create("libs") {
