6d2346ca6ce56a75356c1e3580c8d52b3778e619
Matej Drobnic <matej.drobnic@inova.si>
2024-02-20 08:31:46 +0100

    add dependency analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,6 @@
 // This file is intentionally blank. Please do not add things here unless really necesary in order to stay compatible with
 // project isolation when it comes out (https://gradle.github.io/configuration-cache/#project_isolation)
+
+plugins {
+   id("com.autonomousapps.dependency-analysis") version "1.30.0"
+}
