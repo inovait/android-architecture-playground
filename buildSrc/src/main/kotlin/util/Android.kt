09cf0f3eb98100f8bf5cc456a037b35f23bb577d
Matej Drobnic <matej.drobnic@inova.si>
2024-03-01 07:01:02 +0100

    chore: bump dependencies

--- buildSrc/src/main/kotlin/util/Android.kt
+++ buildSrc/src/main/kotlin/util/Android.kt
@@ -4,6 +4,7 @@ import com.android.build.api.dsl.AndroidResources
 import com.android.build.api.dsl.BuildFeatures
 import com.android.build.api.dsl.BuildType
 import com.android.build.api.dsl.DefaultConfig
+import com.android.build.api.dsl.Installation
 import com.android.build.api.dsl.ProductFlavor
 import com.android.build.gradle.internal.dsl.InternalTestedExtension
 import com.android.build.gradle.internal.utils.KOTLIN_ANDROID_PLUGIN_ID
@@ -21,7 +22,8 @@ fun Project.commonAndroid(
          BuildType,
          DefaultConfig,
          ProductFlavor,
-         AndroidResources
+         AndroidResources,
+         Installation
          >
       >,
 ) {
@@ -31,7 +33,7 @@ fun Project.commonAndroid(
 /**
  * kotlinOptions {} block that can be used without applying specific android plugin
  */
-fun InternalTestedExtension<*, *, *, *, *>.commonKotlinOptions(
+fun InternalTestedExtension<*, *, *, *, *, *>.commonKotlinOptions(
    block: Action<KotlinJvmOptions>,
 ) {
    (this as org.gradle.api.plugins.ExtensionAware).extensions.configure("kotlinOptions", block)
