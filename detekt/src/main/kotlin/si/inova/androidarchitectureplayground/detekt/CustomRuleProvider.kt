e8214c6bc679f5cabf2d871d7fc6f4b6a0150093
Matej Drobnic <matej.drobnic@inova.si>
2024-04-26 09:19:27 +0200

    refactor: enforce action logging use in ViewModels
    using custom detekt rule
    
    (cherry picked from commit 62ad3044f5473cf39c8cf7b597d6300af4933260)

---
+++ detekt/src/main/kotlin/si/inova/androidarchitectureplayground/detekt/CustomRuleProvider.kt
@@ -0,0 +1,19 @@
+package si.inova.androidarchitectureplayground.detekt
+
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.RuleSet
+import io.gitlab.arturbosch.detekt.api.RuleSetProvider
+
+class CustomRuleProvider : RuleSetProvider {
+   override val ruleSetId: String
+      get() = "custom"
+
+   override fun instance(config: Config): RuleSet {
+      return RuleSet(
+         ruleSetId,
+         listOf(
+            UseActionLoggerInViewModels(config)
+         )
+      )
+   }
+}
