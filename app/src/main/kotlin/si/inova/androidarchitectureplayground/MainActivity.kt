8af84726ca42e2bd045a25cab3149b9887e4d010
Matej Drobnic <matej.drobnic@inova.si>
2024-07-05 08:03:01 +0200

    feat: enable edge to edge drawing
    (cherry picked from commit ffbd49f7595e80d2a51cc5367419e7be3921f1e9)

--- app/src/main/kotlin/si/inova/androidarchitectureplayground/MainActivity.kt
+++ app/src/main/kotlin/si/inova/androidarchitectureplayground/MainActivity.kt
@@ -3,6 +3,7 @@ package si.inova.androidarchitectureplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -60,6 +61,7 @@ class MainActivity : ComponentActivity() {
    override fun onCreate(savedInstanceState: Bundle?) {
       (requireNotNull(application) as MyApplication).applicationComponent.inject(this)
       super.onCreate(savedInstanceState)
+      enableEdgeToEdge()
 
       val splashScreen = installSplashScreen()
       splashScreen.setKeepOnScreenCondition { !initComplete }
