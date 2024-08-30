809c67564cc3054aaf04f8a70bbb35cf77acad89
Matej Drobnic <matej.drobnic@inova.si>
2024-08-30 08:58:09 +0200

    test(app-screenshot-test): reduce test name

--- app-screenshot-tests/src/test/kotlin/screenshot/Tests.kt
+++ app-screenshot-tests/src/test/kotlin/screenshot/Tests.kt
@@ -1,8 +1,10 @@
-package si.inova.androidarchitectureplayground.screenshottests
+// Package is intentionally short to reduce image file name
+package screenshot
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalInspectionMode
+import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.DeviceConfig.Companion.PIXEL_5
 import app.cash.paparazzi.Paparazzi
 import com.airbnb.android.showkase.models.Showkase
@@ -20,10 +22,10 @@ import si.inova.androidarchitectureplayground.showkase.getMetadata
 
 @Suppress("JUnitMalformedDeclaration")
 @RunWith(TestParameterInjector::class)
-class ScreenshotTests {
+class Tests {
    @get:Rule
    val paparazzi = Paparazzi(
-      deviceConfig = PIXEL_5,
+      deviceConfig = DeviceConfig.PIXEL_5,
       theme = "android:Theme.Material.Light.NoActionBar",
       maxPercentDifference = 0.0,
       showSystemUi = false,
@@ -38,9 +40,11 @@ class ScreenshotTests {
    }
 
    data class TestKey(val showkaseBrowserComponent: ShowkaseBrowserComponent) {
-      override fun toString(): String {
-         return showkaseBrowserComponent.componentKey
+      val key = with(showkaseBrowserComponent) {
+         componentName + (styleName?.let { "-$it" } ?: "")
       }
+
+      override fun toString(): String = key
    }
 
    @Before
@@ -51,7 +55,7 @@ class ScreenshotTests {
    }
 
    @Test
-   fun launchTests(
+   fun test(
       @TestParameter(valuesProvider = PreviewProvider::class)
       testKey: TestKey,
    ) {
@@ -65,7 +69,7 @@ class ScreenshotTests {
          composable()
       }
       paparazzi.unsafeUpdateConfig(
-         PIXEL_5.copy(
+         DeviceConfig.PIXEL_5.copy(
             nightMode = NightMode.NIGHT
          )
       )
