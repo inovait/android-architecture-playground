e3d0f22bfa06c64479772a8a925e2d1bf73548c7
Matej Drobnic <matej.drobnic@inova.si>
2024-08-30 09:50:43 +0200

    build: fix random compose guard build cache errors

--- buildSrc/src/main/kotlin/compose.gradle.kts
+++ buildSrc/src/main/kotlin/compose.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.accessors.dm.LibrariesForLibs
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
 import util.commonAndroid
 import util.commonKotlinOptions
 
@@ -22,6 +23,48 @@ commonAndroid {
             "${rootDir.absolutePath}/config/global_compose_stable_classes.txt"
       )
    }
+}
+
+composeGuardCheck {
+   // Dynamic property detection is error prone on debug builds (and we are only building debug builds for PR changes)
+   // See https://chrisbanes.me/posts/composable-metrics/#default-parameter-expressions-that-are-dynamic
+   errorOnNewDynamicProperties = false
+
+   // We don't care about unstable classes, only unstable Composables
+   errorOnNewUnstableClasses = false
+}
+
+// Workaround for https://github.com/j-roskopf/ComposeGuard/issues/47 - manually register compose reports and metrics folder
+composeGuard {
+   configureKotlinTasks = false
+}
+
+// List of all tasks in this module that compile compose stuff (excluding KSP etc.)
+val composeCompileTasks = listOf("compileDebugKotlin", "compileReleaseKotlin")
+
+val composeReportsFolder = composeGuardCheck.outputDirectory.get()
+project.tasks.named { composeCompileTasks.contains(it) }.withType<KotlinCompile<*>>().configureEach {
+   kotlinOptions {
+      freeCompilerArgs += listOf(
+         "-P",
+         "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+            "$composeReportsFolder"
+      )
+      freeCompilerArgs += listOf(
+         "-P",
+         "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
+            "$composeReportsFolder"
+      )
+   }
+
+   outputs.dir(composeReportsFolder)
+}
+
+tasks.register<Copy>("generateComposeGuardBaseline") {
+   from(composeReportsFolder)
+   into(composeGuardGenerate.outputDirectory)
+
+   dependsOn("compileDebugKotlin")
 }
 
 dependencies {
