a77d431b3598cbcc1410c285387d9a160bce37a0
Matej Drobnič <matej@matejdro.com>
2025-01-28 06:44:03 +0100

    build(common-retrofit): add missing dependencies

--- common-retrofit/android/build.gradle.kts
+++ common-retrofit/android/build.gradle.kts
@@ -6,8 +6,10 @@ plugins {
 dependencies {
    api(projects.commonRetrofit)
    api(libs.dispatch)
+   api(libs.kotlinova.core)
    api(libs.moshi)
    api(libs.okhttp)
+   api(libs.kotlinova.retrofit)
    api(libs.certificateTransparency)
 
    implementation(libs.kotlin.coroutines)
