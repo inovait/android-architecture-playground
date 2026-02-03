44c77affddae776d93cd4466afb59a1f3a24855e
Matej Drobnic <matej.drobnic@inova.si>
2026-02-03 10:22:38 +0100

    chore: bump detekt

--- detekt/src/main/kotlin/si/inova/androidarchitectureplayground/detekt/CustomRuleProvider.kt
+++ detekt/src/main/kotlin/si/inova/androidarchitectureplayground/detekt/CustomRuleProvider.kt
@@ -1,10 +1,11 @@
 package si.inova.androidarchitectureplayground.detekt
 
 import dev.detekt.api.RuleSet
+import dev.detekt.api.RuleSetId
 import dev.detekt.api.RuleSetProvider
 
 class CustomRuleProvider : RuleSetProvider {
-   override val ruleSetId = RuleSet.Id("custom")
+   override val ruleSetId = RuleSetId("custom")
 
    override fun instance(): RuleSet {
       return RuleSet(
