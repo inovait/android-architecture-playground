f553609d6dc56266901fd93e7cb23a3d14be85d4
matejdrotest <servicesgithub2@matejdro.com>
2026-02-05 10:09:03 +0100

    Update generate-screenshots.sh

--- config/generate-screenshots.sh
+++ config/generate-screenshots.sh
@@ -21,6 +21,6 @@ then
   git config user.email 41898282+github-actions[bot]@users.noreply.github.com
   git commit -m "chore: add new screenshot tests"
   # Pushing large amounts of screenshot something fails. Push LFS screenshots first.
-  git lfs push --all origin
+  git lfs push origin HEAD
   git push origin HEAD:$1
 fi
