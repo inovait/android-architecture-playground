b54e50d47a8e7f8317d3900c1a5683a6896a4e2c
Matej Drobnic <matej.drobnic@inova.si>
2026-04-03 14:41:34 +0200

    refactor: fix compose stability
    (cherry picked from commit 3ec0a51f2b1bb19260dc9be3dcb26d742587df72)

--- buildSrc/src/main/kotlin/navigationApi.gradle.kts
+++ buildSrc/src/main/kotlin/navigationApi.gradle.kts
@@ -1,15 +1,22 @@
-import com.autonomousapps.DependencyAnalysisExtension
-import com.autonomousapps.DependencyAnalysisSubExtension
 import org.gradle.accessors.dm.LibrariesForLibs
 
 val libs = the<LibrariesForLibs>()
 
 plugins {
    id("serialization")
+
+   // Run compose plugin to get @StabilityInferred on all data classes in the API folder
+   id("org.jetbrains.kotlin.plugin.compose")
 }
 
 dependencies {
    if (name != "common-navigation") {
       add("implementation", project(":common-navigation"))
+   }
+
+   add("compileOnly", libs.androidx.compose.runtime)
+
+   plugins.withId("test-fixtures") {
+      add("testFixturesCompileOnly", libs.androidx.compose.runtime)
    }
 }
