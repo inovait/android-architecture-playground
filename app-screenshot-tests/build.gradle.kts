03aeb784b0c4fd162c36e63814f679a0187105f4
Matej Drobnic <matej.drobnic@inova.si>
2026-05-29 07:46:49 +0200

    test(app-screenshot-tests): remove old workarounds
    new paparazzi has fixed them

--- app-screenshot-tests/build.gradle.kts
+++ app-screenshot-tests/build.gradle.kts
@@ -12,31 +12,10 @@ android {
    testOptions {
       unitTests.all {
          it.useJUnit()
-         it.reports.html.required = false
 
          val numSplits = 2 // How many TestsX classes are there
          it.maxParallelForks = minOf(Runtime.getRuntime().availableProcessors(), numSplits)
          it.systemProperty("numSplits", numSplits)
-      }
-   }
-}
-
-plugins.withId("app.cash.paparazzi") {
-   // Defer until afterEvaluate so that testImplementation is created by Android plugin.
-   afterEvaluate {
-      dependencies.constraints {
-         add("testImplementation", "com.google.guava:guava") {
-            attributes {
-               attribute(
-                  TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
-                  objects.named(TargetJvmEnvironment::class, TargetJvmEnvironment.STANDARD_JVM)
-               )
-            }
-            because(
-               "LayoutLib and sdk-common depend on Guava's -jre published variant." +
-                  "See https://github.com/cashapp/paparazzi/issues/906."
-            )
-         }
       }
    }
 }
