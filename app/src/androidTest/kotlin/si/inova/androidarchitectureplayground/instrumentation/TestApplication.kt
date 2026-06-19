ea61060e92e62c899f31e48067489a9228d7e4f7
Matej Drobnic <matej.drobnic@inova.si>
2026-06-19 07:17:26 +0200

    test(app): remove keeper
    it has not been updated for a while and is not supported on AGP 9

--- app/src/androidTest/kotlin/si/inova/androidarchitectureplayground/instrumentation/TestApplication.kt
+++ app/src/androidTest/kotlin/si/inova/androidarchitectureplayground/instrumentation/TestApplication.kt
@@ -3,9 +3,18 @@ package si.inova.androidarchitectureplayground.instrumentation
 import dev.zacsweers.metro.createGraphFactory
 import si.inova.androidarchitectureplayground.MyApplication
 import si.inova.androidarchitectureplayground.di.ApplicationGraph
+import java.time.Duration
+import java.util.TimeZone
 
 class TestApplication : MyApplication() {
    override val applicationGraph: ApplicationGraph by lazy {
       createGraphFactory<TestApplicationGraph.Factory>().create(this)
+   }
+
+   init {
+      // We need to make these calls as a workaround for the https://issuetracker.google.com/issues/277672742
+      Duration.ofMillis(0L)
+      Duration.ofSeconds(0L).toMillis()
+      TimeZone.getTimeZone("UTC")
    }
 }
